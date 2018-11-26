9c2aec2788c2f02e01bf4318f5fe757c6af6ad69
Alisson <alissonferreiraluz@gmail.com>
2018-11-26 00:15:49 -0200

    Tarefa 3

--- chicago_bikeshare_pt.py
+++ chicago_bikeshare_pt.py
@@ -44,18 +44,20 @@ input("Aperte Enter para continuar...")
 def column_to_list(data, index):
     column_list = []
     # Dica: Você pode usar um for para iterar sobre as amostras, pegar a feature pelo seu índice, e dar append para uma lista
+    for amostra in data:
+        amostra_list = list(amostra.values())
+        column_list.append(amostra_list[index])
     return column_list
 
 
 # Vamos checar com os gêneros se isso está funcionando (apenas para os primeiros 20)
 print("\nTAREFA 3: Imprimindo a lista de gêneros das primeiras 20 amostras")
-lista = list(data_list[1].values())
-print(lista[-2])
 print(column_to_list(data_list, -2)[:20])
 
 # ------------ NÃO MUDE NENHUM CÓDIGO AQUI ------------
 assert type(column_to_list(data_list, -2)) is list, "TAREFA 3: Tipo incorreto retornado. Deveria ser uma lista."
-assert len(column_to_list(data_list, -2)) == 1551505, "TAREFA 3: Tamanho incorreto retornado."
+assert len(column_to_list(data_list, -2)) == 999, "TAREFA 3: Tamanho incorreto retornado."
+#assert len(column_to_list(data_list, -2)) == 1551505, "TAREFA 3: Tamanho incorreto retornado."
 assert column_to_list(data_list, -2)[0] == "" and column_to_list(data_list, -2)[1] == "Male", "TAREFA 3: A lista não coincide."
 # -----------------------------------------------------
 
